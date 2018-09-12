ce9b568d8ee18dd49c242973de8a94d1c53bd4d0
curioswati <jaiswalswati94@gmail.com>
2018-09-12 19:09:27 +0530

    updates scraper to collect all ddos for a given treasury
    * adds new class that doesn't use monthly/yearly intervals, instead
    collect for a single date range

--- scraper/spiders/dataset_collector.py
+++ scraper/spiders/dataset_collector.py
@@ -8,6 +8,7 @@ from urllib.parse import urlencode
 import pandas as pd
 import scrapy
 from scrapy.spidermiddlewares.httperror import HttpError
+from scrapy.exceptions import CloseSpider
 
 from scraper.settings import settings
 
@@ -17,7 +18,7 @@ def clean_text(text):
 
 
 def make_file_name(attrs):
-    filename = '{query}_{treasury}_{date}.csv'.format(**attrs)
+    filename = '{query}_{treasury}_{ddo}_{date}.csv'.format(**attrs)
     filename = re.sub(r',+', '', filename).replace(' ', '_').replace('/', '_')
     return filename
 
@@ -42,7 +43,6 @@ def create_date_ranges(begin, stop):
 class DatasetCollector(scrapy.Spider):
     allowed_domains = ['himkosh.hp.nic.in']
 
-    ddo_code = '500'
     datasets_path = get_datasets_path()
 
     def parse(self, response):
@@ -66,35 +66,46 @@ class DatasetCollector(scrapy.Spider):
 
         # for each year for each query for each treasury, make requests for datasets.
         for treasury in treasuries[1:]:
-            treasury_id = treasury.css('::attr(value)').extract_first() + '-' + self.ddo_code
+            treasury_id = treasury.css('::attr(value)').extract_first()
             treasury_name = treasury.css('::text').extract_first()
             treasury_name = clean_text(treasury_name)
 
-            for query in queries[1:]:
-                query_id = query.css('::attr(value)').extract_first()
-                query_name = query.css('::text').extract_first()
-                query_name = clean_text(query_name)
+            ddo_file = open(
+                os.path.join(self.datasets_path, '{}_ddo_codes.csv'.format(treasury_id))
+            )
+            ddo_code_reader = csv.reader(ddo_file)
 
-                for start, end in zip(start_dates, end_dates):
-                    query_params = {
-                        'from_date': start,
-                        'To_date': end,
-                        'ddlquery': query_id,
-                        'HODCode': treasury_id,
-                        'Str': query_name
-                    }
+            next(ddo_code_reader)
+            for ddo in ddo_code_reader:
+                ddo_code = ddo[0]
 
-                    filename = make_file_name({'query': query_name,
-                                               'treasury': treasury_name,
-                                               'date': '{}-{}'.format(start, end)})
-                    filepath = os.path.join(self.datasets_path, filename)
+                for query in queries[1:]:
+                    query_id = query.css('::attr(value)').extract_first()
+                    query_name = query.css('::text').extract_first()
+                    query_name = clean_text(query_name)
 
-                    # don't request the same dataset again if it's already collected previously
-                    if not os.path.exists(filepath):
-                        yield scrapy.Request(
-                            self.query_url.format(urlencode(query_params)), self.parse_dataset,
-                            errback=self.handle_err, meta={'filepath': filepath}
-                        )
+                    for start, end in zip(start_dates, end_dates):
+                        query_params = {
+                            'from_date': start,
+                            'To_date': end,
+                            'ddlquery': query_id,
+                            'HODCode': '{}-{}'.format(treasury_id, ddo_code),
+                            'Str': query_name
+                        }
+
+                        filename = make_file_name({'query': query_name,
+                                                   'treasury': treasury_name,
+                                                   'ddo': ddo_code,
+                                                   'date': '{}-{}'.format(start, end)})
+                        filepath = os.path.join(self.datasets_path, filename)
+
+                        # don't request the same dataset again if it's already collected previously
+                        if not os.path.exists(filepath):
+                            yield scrapy.Request(
+                                self.query_url.format(urlencode(query_params)), self.parse_dataset,
+                                errback=self.handle_err, meta={'filepath': filepath}
+                            )
+            ddo_file.close()
 
     def handle_err(self, failure):
         if failure.check(HttpError):
@@ -140,7 +151,64 @@ class DatasetCollector(scrapy.Spider):
                 writer.writerow(observation)
 
 
-class ExpendituresSpider(DatasetCollector):
+class DatasetCollector2(DatasetCollector):
+
+    def parse(self, response):
+        '''
+        Collect queryable params and make dataset queries.
+        '''
+
+        if not hasattr(self, 'start') and not hasattr(self, 'end'):
+            raise CloseSpider('No date range given!')
+
+        # collect last query that gives consolidated data.
+        query = response.css('#ddlQuery option')[10]
+
+        query_id = query.css('::attr(value)').extract_first()
+        query_name = query.css('::text').extract_first()
+        query_name = clean_text(query_name)
+
+        # collect all treasury names from dropdown.
+        treasuries = response.css('#cmbHOD option')
+
+        # for each treasury for each ddo, make requests for datasets for the given date range and query.  # noqa
+        for treasury in treasuries[1:]:
+            treasury_id = treasury.css('::attr(value)').extract_first()
+            treasury_name = treasury.css('::text').extract_first()
+            treasury_name = clean_text(treasury_name)
+
+            ddo_file = open(
+                os.path.join(self.datasets_path, '{}_ddo_codes.csv'.format(treasury_id))
+            )
+            ddo_code_reader = csv.reader(ddo_file)
+
+            next(ddo_code_reader)
+            for ddo in ddo_code_reader:
+                ddo_code = ddo[0]
+
+                query_params = {
+                    'from_date': self.start,
+                    'To_date': self.end,
+                    'ddlquery': query_id,
+                    'HODCode': '{}-{}'.format(treasury_id, ddo_code),
+                    'Str': query_name
+                }
+                filename = make_file_name({'query': query_name,
+                                           'treasury': treasury_name,
+                                           'ddo': ddo_code,
+                                           'date': '{}-{}'.format(self.start, self.end)})
+                filepath = os.path.join(self.datasets_path, filename)
+
+                # don't request the same dataset again if it's already collected previously
+                if not os.path.exists(filepath):
+                    yield scrapy.Request(
+                        self.query_url.format(urlencode(query_params)), self.parse_dataset,
+                        errback=self.handle_err, meta={'filepath': filepath}
+                    )
+            ddo_file.close()
+
+
+class ExpendituresSpider(DatasetCollector2):
     name = 'expenditures'
 
     # this page contains all the populating info.
